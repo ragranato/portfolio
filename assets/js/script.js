fb0d39b8ead38668191a2143e58433d412439499
ragranato <robert.granato@gmail.com>
2023-05-20 16:47:47 -0400

    added photo, experience, skills

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 const toggle = (element) => {
     const about = document.getElementById(element);
     if (about.style.display === 'none') {
-        about.style.display = 'block';
+        about.style.display = 'flex';
     } else {
         about.style.display = 'none';
     }
